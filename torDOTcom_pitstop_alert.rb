b41f712836f60526179a6160e86a622ddb0c7c40
nelliemckesson <nellie.mckesson@macmillan.com>
2015-05-21 14:09:58 -0400

    fixing alert

--- torDOTcom_pitstop_alert.rb
+++ torDOTcom_pitstop_alert.rb
@@ -1,3 +1,5 @@
+require_relative '../bookmaker/header.rb'
+
 # Add a notice to the conversion dir warning that the process is in use
 File.open("#{Bkmkr::Paths.alert}", 'w') do |output|
 	output.write "The conversion processor is currently running. Please do not submit any new files or images until the process completes."
