ed8156f0b217272495366517348ac208e3c56144
nelliemckesson <nellie.mckesson@macmillan.com>
2015-05-21 13:38:57 -0400

    fixing output file

--- torDOTcom_pitstop_output.rb
+++ torDOTcom_pitstop_output.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 input_file = ARGV[0]
 input_file = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).join(File::SEPARATOR)
 filename = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop.split("-").pop
-isbn = filename.split("_").shift
+isbn = filename.split(".").shift
 project_dir = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop.split("-").shift
 final_dir = File.join("S:", "bookmaker", project_dir, "done", "#{isbn}", "#{filename}")
 alert = File.join("S:", "bookmaker", project_dir, "IN_USE_PLEASE_WAIT.txt")
