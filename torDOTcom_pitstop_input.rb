244e31f09b080b324435bf415d98d35898e97440
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-11 13:15:42 -0400

    adding switch for staging server

--- torDOTcom_pitstop_input.rb
+++ torDOTcom_pitstop_input.rb
@@ -3,12 +3,12 @@ require 'fileutils'
 require_relative '../bookmaker/core/header.rb'
 require_relative '../bookmaker/core/metadata.rb'
 
-input_filename = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "#{Metadata.pisbn}_POD.pdf")
-pitstop_filename = File.join(pitstop_dir, "#{Bkmkr::Project.project_dir}_#{Bkmkr::Project.stage_dir}-#{Metadata.pisbn}.pdf")
 if File.file?("#{Bkmkr::Paths.resource_dir}/staging.txt")
 	pitstop_dir = File.join("P:", "torDOTcom_POD_staging", "input")
 else
 	pitstop_dir = File.join("P:", "torDOTcom_POD", "input")
 end
+input_filename = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "#{Metadata.pisbn}_POD.pdf")
+pitstop_filename = File.join(pitstop_dir, "#{Bkmkr::Project.project_dir}_#{Bkmkr::Project.stage_dir}-#{Metadata.pisbn}.pdf")
 
 FileUtils.mv(input_filename, pitstop_filename)
