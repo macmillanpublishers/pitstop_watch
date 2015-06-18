744cb91883b9e0486ede931001c1a1f4313a9754
nelliemckesson <nellie.mckesson@macmillan.com>
2015-06-18 16:31:33 -0400

    removing ref to project dir

--- torDOTcom_pitstop_input.rb
+++ torDOTcom_pitstop_input.rb
@@ -3,12 +3,19 @@ require 'fileutils'
 require_relative '../bookmaker/core/header.rb'
 require_relative '../bookmaker/core/metadata.rb'
 
+configfile = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
+file = File.read(configfile)
+data_hash = JSON.parse(file)
+
+project_dir = data_hash['project']
+stage_dir = data_hash['stage']
+
 if File.file?("#{Bkmkr::Paths.resource_dir}/staging.txt")
 	pitstop_dir = File.join("P:", "torDOTcom_POD_staging", "input")
 else
 	pitstop_dir = File.join("P:", "torDOTcom_POD", "input")
 end
 input_filename = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "#{Metadata.pisbn}_POD.pdf")
-pitstop_filename = File.join(pitstop_dir, "#{Bkmkr::Project.project_dir}_#{Bkmkr::Project.stage_dir}-#{Metadata.pisbn}.pdf")
+pitstop_filename = File.join(pitstop_dir, "#{project_dir}_#{stage_dir}-#{Metadata.pisbn}.pdf")
 
 FileUtils.mv(input_filename, pitstop_filename)
