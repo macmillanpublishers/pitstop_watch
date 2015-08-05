b5890792777000730ca91cb6449b6fd669a99949
nelliemckesson <nellie.mckesson@macmillan.com>
2015-08-05 10:48:25 -0400

    adding support for other project folders

--- torDOTcom_pitstop_input.rb
+++ torDOTcom_pitstop_input.rb
@@ -11,9 +11,9 @@ project_dir = data_hash['project']
 stage_dir = data_hash['stage']
 
 if File.file?("#{Bkmkr::Paths.resource_dir}/staging.txt")
-	pitstop_dir = File.join("P:", "torDOTcom_POD_staging", "input")
+	pitstop_dir = File.join("P:", "#{project_dir}_POD_staging", "input")
 else
-	pitstop_dir = File.join("P:", "torDOTcom_POD", "input")
+	pitstop_dir = File.join("P:", "#{project_dir}_POD", "input")
 end
 input_filename = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "#{Metadata.pisbn}_POD.pdf")
 pitstop_filename = File.join(pitstop_dir, "#{project_dir}_#{stage_dir}-#{Metadata.pisbn}.pdf")
