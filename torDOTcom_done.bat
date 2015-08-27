9abe037a621ac1cbf9af387cef5f98a4cdfc64ca
nelliemckesson <nellie.mckesson@macmillan.com>
2015-08-27 16:17:46 -0400

    adding bat deploy file

--- torDOTcom_done.bat
+++ torDOTcom_done.bat
@@ -1,1 +1,1 @@
-C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\pitstop_watch\torDOTcom_pitstop_output.rb '%1'
+C:\Ruby193\bin\ruby.exe S:\resources\bookmaker_scripts\pitstop_watch\torDOTcom_pitstop_output.rb '%1' >> log.txt 2>&1
