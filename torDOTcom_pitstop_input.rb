365ff15aee131301cc79fca7c185216b1ab7d13c
matt.retzer <matthew.retzer@macmillan.com>
2017-01-06 10:49:04 -0500

    added jsonlogging for pitstop_input and pitstop_output.rb

--- torDOTcom_pitstop_input.rb
+++ torDOTcom_pitstop_input.rb
@@ -3,34 +3,89 @@ require 'fileutils'
 require_relative '../bookmaker/core/header.rb'
 require_relative '../bookmaker/core/metadata.rb'
 
-configfile = File.join(Bkmkr::Paths.project_tmp_dir, "config.json")
-file = File.read(configfile)
-data_hash = JSON.parse(file)
+# ---------------------- VARIABLES
+local_log_hash, @log_hash = Bkmkr::Paths.setLocalLoghash
 
+testing_value_file = File.join(Bkmkr::Paths.resource_dir, "staging.txt")
+
+# ---------------------- METHODS
+def readConfigJson(logkey='')
+  data_hash = Mcmlln::Tools.readjson(Metadata.configfile)
+  return data_hash
+rescue => logstring
+  return {}
+ensure
+  Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def checkStaging(file, logkey='')
+	if File.file?(file)
+		staging = "_staging"
+	else
+		staging = ""
+	end
+	return staging
+rescue => logstring
+	return ''
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+def setPitstopDir(this_pitstop_dir, staging, logkey='')
+	if File.exist?(this_pitstop_dir)
+		pitstop_dir = this_pitstop_dir
+	else
+		pitstop_dir = File.join("P:", "SMP_POD#{staging}", "input")
+	end
+	return pitstop_dir
+rescue => logstring
+	return ''
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+## wrapping a Mcmlln::Tools method in a new method for this script; to return a result for json_logfile
+def moveFileToPitstopDir(file, dest, logkey='')
+	Mcmlln::Tools.moveFile(file, dest)
+rescue => logstring
+ensure
+	Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
+end
+
+# ---------------------- PROCESSES
+data_hash = readConfigJson('read_config_json')
+
+##### local definition(s) based on data from config.json
 project_dir = data_hash['project']
 stage_dir = data_hash['stage']
 
-if File.file?("#{Bkmkr::Paths.resource_dir}/staging.txt")
-	staging = "_staging"
-else
-	staging = ""
-end
+# see if we're on the staging server
+staging = checkStaging(testing_value_file, 'check_if_on_staging')
+@log_hash['staging_value'] = staging
 
+# set pitstop dir, including staging as needed
 this_pitstop_dir = File.join("P:", "#{project_dir}_POD#{staging}", "input")
 
-if File.exist?(this_pitstop_dir)
-	pitstop_dir = this_pitstop_dir
-else
-	pitstop_dir = File.join("P:", "SMP_POD#{staging}", "input")
-end
+# choose pitstop_dir by project, if folder doesn't exist for this project default to SMP_POD
+pitstop_dir = setPitstopDir(this_pitstop_dir, staging, 'set_pitstop_dir')
+
 input_filename = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "#{Metadata.pisbn}_POD.pdf")
 pitstop_filename = File.join(pitstop_dir, "#{project_dir}_#{stage_dir}-#{Metadata.pisbn}.pdf")
+@log_hash['pitstop_filename'] = pitstop_filename
 
-FileUtils.mv(input_filename, pitstop_filename)
+moveFileToPitstopDir(input_filename, pitstop_filename, 'move_file_to_pitstop_dir')
 
-# LOGGING
-
-# Printing the test results to the log file
-File.open(Bkmkr::Paths.log_file, 'a+') do |f|
-	f.puts "----- SENT PDF TO PITSTOP"
+# ---------------------- LOGGING
+# wrapping this legacy log in a begin block so it doesn't hose travis tests.
+begin
+	# Printing the test results to the log file
+	File.open(Bkmkr::Paths.log_file, 'a+') do |f|
+		f.puts "----- SENT PDF TO PITSTOP"
+	end
+rescue => e
+  puts '(Ignore for unit-tests:) ERROR encountered in process block: ', e
 end
+
+# Write json log:
+Mcmlln::Tools.logtoJson(@log_hash, 'completed', Time.now)
+Mcmlln::Tools.write_json(local_log_hash, Bkmkr::Paths.json_log)
