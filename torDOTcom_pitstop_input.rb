23d859423cab03f34f4a39a41a059ca64a9132b9
Matt Retzer <7025870+mattretzer@users.noreply.github.com>
2020-09-23 11:46:40 -0400

    re-added staging folder parameters and strings, commented, for easy pivot if 1 pitstop folder becomes unavail

--- torDOTcom_pitstop_input.rb
+++ torDOTcom_pitstop_input.rb
@@ -24,24 +24,33 @@ end
 def checkStaging(staging_file, prod_pitstop_drive, stg_pitstop_drive, logkey='')
   if File.file?(staging_file)
     ps_drive_letter = stg_pitstop_drive
+    staging = "_staging"
   else
     ps_drive_letter = prod_pitstop_drive
+    staging = ""
   end
-  return ps_drive_letter
+  return ps_drive_letter, staging
 rescue => logstring
   return ''
 ensure
   Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
 end
 
-def setPitstopDir(project_dir, ps_drive_letter, logkey='')
+def setPitstopDir(project_dir, ps_drive_letter, staging, logkey='')
   # set pitstop dir
   this_pitstop_dir = File.join(ps_drive_letter, "#{project_dir}_POD", "input")
+  # this_pitstop_dir = File.join(ps_drive_letter, "#{project_dir}_POD#{staging}", "done")
+  ### leaving this commented, alternate value ^^^ including 'staging' value,
+  ###   in case, during course of pitstop development, one env becomes unusable:
+  ###   we can simply uncomment to have both bkmkr servers share one pitstop server again.
+  ### (ditto line 53 below)
+
   # fallback on smp if no folder for this project_dir
   if File.exist?(this_pitstop_dir)
     pitstop_dir = this_pitstop_dir
   else
     pitstop_dir = File.join(ps_drive_letter, "SMP_POD", "input")
+    # pitstop_dir = File.join(ps_drive_letter, "SMP_POD#{staging}", "input")
   end
   return pitstop_dir
 rescue => logstring
@@ -66,11 +75,12 @@ project_dir = data_hash['project']
 stage_dir = data_hash['stage']
 
 # see if we're on the staging server
-ps_drive_letter = checkStaging(testing_value_file, prod_pitstop_drive, stg_pitstop_drive, 'get_prd-or-stg_driveletter')
+ps_drive_letter, staging = checkStaging(testing_value_file, prod_pitstop_drive, stg_pitstop_drive, 'get_prd-or-stg_driveletter')
 @log_hash['ps_drive_letter'] = ps_drive_letter
+@log_hash['staging'] = staging
 
 # choose pitstop_dir by project, if folder doesn't exist for this project default to SMP_POD
-pitstop_dir = setPitstopDir(project_dir, ps_drive_letter, 'set_pitstop_dir')
+pitstop_dir = setPitstopDir(project_dir, ps_drive_letter, staging, 'set_pitstop_dir')
 
 input_filename = File.join(Metadata.final_dir, "#{Metadata.pisbn}_POD.pdf")
 pitstop_filename = File.join(pitstop_dir, "#{project_dir}_#{stage_dir}-#{Metadata.pisbn}.pdf")
